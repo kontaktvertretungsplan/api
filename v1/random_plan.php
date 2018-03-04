1c2ef52f3a974b6e90900643337effd82287a328
clerie <clemens@milchinsel.de>
2018-03-04 13:03:03 +0100

    Beispiel API (Zufälliger Stundenplan) erweitert

--- v1/random_plan.php
+++ v1/random_plan.php
@@ -14,10 +14,9 @@ Kontakt:Vertretungsplan Stundenplan Api
 Besipiel Stundenplan Generator zum testen
 */
 
-$year = $_GET['year'];
-$month = $_GET['month'];
-$day = $_GET['day'];
+// Inhalte
 
+// Klassen
 $CLASSES = [
 	"5",
 	"6",
@@ -27,10 +26,73 @@ $CLASSES = [
 	"10",
 ];
 
+// Anmerkungen
+$NOTICES = [
+	"Klausurenphase: Bitte Ruhe in den Pausen",
+	"Schulleitung heute abesend",
+	"Klasse 6d auf Klassenfahrt",
+	"Heute gibts Kuchen in der Pause",
+	
+];
+
+
+
+// Inputs
+$year = $_GET['year'];
+$month = $_GET['month'];
+$day = $_GET['day'];
+
+
+
+// Ausgabevariable
 $out = [];
 
-for($lesson = 1; $lesson <= 6; $lesson++) {
+// Systemstatus
+$out["ok"] = true;
+
+// Systeminfo
+$out["info"] = [
+	"api" => [
+		"version" => "1",	
+	],
+];
+
+// Inhalte
+
+// Stundenplanstatus
+$out["result"]["status"] = "ok";
+
+// Datum
+$out["result"]["date"] = [
+	"year" => $year,
+	"month" => $month,
+	"day" => $day,
+];
+
+// Stundenplan Inhalte
+/*for($lesson = 1; $lesson <= 6; $lesson++) {
 	foreach($CLASSES as $class) {
 		
 	}
+}*/
+
+// Anmerkungen
+$notice = [];
+for($n = 0; $n <= rand(0, round((count($NOTICES)-1)/2)); $n++) {
+	while(!isset($notice[$n])) {
+		$n = rand(0, count($NOTICES)-1);
+		if(!in_array($NOTICES[$n], $notice)) {
+			$notice[] = $NOTICES[$n];
+		}
+	}
 }
+
+foreach($notice as $n => $note) {
+	$out["note"][$n] = [
+		"0" => $note,
+	];
+}
+
+header("Content-Type: application/json");
+
+echo json_encode($out);
